93dae50f8243a4e58814070232275fffe1308a6b
AleksStek <AleksStek@yandex.ru>
2023-01-17 13:45:14 +0700

    config vite for build npm lib

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,23 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'UnitUIKit',
+      fileName: (format) => `unit-ui-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
